b6d564cc2fe458671bcd8a40b9f2a48074efcf64
Bobwizardlad <bobwizardlad@gmail.com>
2024-01-02 14:54:14 -0600

    Cursor Restructuring
    I fell into treating the cursor like a player character and made all actions center on it. This worked fine until sync with the grid began which caused me to signal up to the game state.
    
    I made the decision to restructure and make player input happen at the game state and call down to cursor to move. All the logic is happening in the game state (as it should) and cursor is merely a graphical element that moves on command.
    
    Still looking to bound cursor within the grid. Logic is better structured to manage this
    
    *Input delay was shortened a bit more.
    *Game display was changed to stretch when widow is enlarged.
    *Renamed Input-Delay in GameState.cs to Input_Is_Delay for better readability.
    *Added readable sections to GameState.cs, better readability.

--- Dice Tactics/Scripts/Cursor.cs
+++ Dice Tactics/Scripts/Cursor.cs
@@ -4,7 +4,6 @@ using System;
 [GlobalClass]
 public partial class Cursor : Node2D
 {
-	private bool Input_Delay = false;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -14,43 +13,29 @@ public partial class Cursor : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (!Input_Delay)
-		{
-			if (Input.IsActionPressed("CursorUp"))
-			{
-				MoveLocalY(-8);
-				MoveLocalX(16);
-				ActivateInputDelay();
-			}
-			else if (Input.IsActionPressed("CursorDown"))
-			{
-				MoveLocalY(8);
-				MoveLocalX(-16);
-				ActivateInputDelay();
-			}
-			if (Input.IsActionPressed("CursorLeft"))
-			{
-				MoveLocalY(-8);
-				MoveLocalX(-16);
-				ActivateInputDelay();
-			}
-			else if (Input.IsActionPressed("CursorRight"))
-			{
-				MoveLocalY(8);
-				MoveLocalX(16);
-				ActivateInputDelay();
-			}
-		}
 	}
 
-	public void _InputDelayRefresh()
+	public void Cursor_Down()
 	{
-		Input_Delay = false;
+		MoveLocalY(8);
+		MoveLocalX(-16);
 	}
-	
-	public void ActivateInputDelay()
+
+	public void Cursor_Up()
 	{
-		Input_Delay = true;
-		GetNode<Timer>("InputDelay").Start();
+		MoveLocalY(-8);
+		MoveLocalX(16);
+	}
+
+	public void Cursor_Left()
+	{
+		MoveLocalY(-8);
+		MoveLocalX(-16);
+	}
+
+	public void Cursor_Right()
+	{
+		MoveLocalY(8);
+		MoveLocalX(16);
 	}
 }
--- Dice Tactics/Scripts/GameState.cs
+++ Dice Tactics/Scripts/GameState.cs
@@ -3,36 +3,77 @@ using System;
 
 public partial class GameState : Node
 {
+	// --------------------------------------------- VARIABLES -----------------------------------------
+
 	// Editable grid size properties.
 	[Export]
 	private int Grid_X = 16;
 	[Export]
 	private int Grid_Y = 16;
+
+	private Cursor Cursor;
+
+	// Flag for if the cursor is currently being delayed
+	private bool Input_Is_Delay;
+	// Cursor Tile
+	private int Cursor_Pos;
 	// # of tiles.
 	private int Grid_Size;
 	// Grid array to store game state.
     private GridTile[] GameBoard;
 	// Called when the node enters the scene tree for the first time.
+	
+	// --------------------------------------------- BUILT-INS -----------------------------------------
+
 	public override void _Ready()
 	{
+		Cursor = GetNode<Cursor>("TileMap/Cursor");
+
 		Grid_Size = Grid_X * Grid_Y;
+
+		Input_Is_Delay = false;
 
 		// Build the virtual grid using config X and Y.
 		GameBoard = new GridTile[Grid_Size];
 		for (int index = 0; index < Grid_Size; index++)
 		{
-			GameBoard[index].Cursor = false;
-			GameBoard[index].Board_Pos = 
+			GameBoard[index].Is_Cursor = false;
 		}
 
 		// Place cursor on first tile.
-		GameBoard[0].Cursor = true;
+		GameBoard[0].Is_Cursor = true;
+		Cursor_Pos = 0;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (!Input_Is_Delay)
+		{
+			if (Input.IsActionPressed("CursorUp"))
+			{
+				Cursor.Cursor_Up();
+				ActivateInputDelay();
+			}
+			else if (Input.IsActionPressed("CursorDown"))
+			{
+				Cursor.Cursor_Down();
+				ActivateInputDelay();
+			}
+			if (Input.IsActionPressed("CursorLeft"))
+			{
+				Cursor.Cursor_Left();
+				ActivateInputDelay();
+			}
+			else if (Input.IsActionPressed("CursorRight"))
+			{
+				Cursor.Cursor_Right();
+				ActivateInputDelay();
+			}
+		}
 	}
+
+	// --------------------------------------------- STRUCTS -----------------------------------------
 
 	// GridItem struct to store grid state.
     struct GridTile
@@ -41,18 +82,48 @@ public partial class GameState : Node
         // Hazard
         // Type
 		public Vector2 Board_Pos;
-        public bool Cursor;
+        public bool Is_Cursor;
 
         public GridTile()
         {
-            Cursor = false;
+            Is_Cursor = false;
 			Board_Pos = new Vector2(0,0);
         }
 		public GridTile(bool cursor, Vector2 board_pos)
 		{
-			Cursor = cursor;
+			Is_Cursor = cursor;
 			Board_Pos = board_pos;
 		}
     }
 
+	// --------------------------------------------- METHODS -----------------------------------------
+
+	// Function to handle grid motion
+	public void _On_Cursor_Move(int x, int y)
+	{
+		// Precalc
+
+		// Error Catch
+		if (!(Cursor_Pos + (y * Grid_X) + x >= Grid_Size))
+		{
+			return;
+		}
+
+		// Y Motion
+
+		// X Motion
+
+		// Update Graphics
+	}
+
+	public void _InputDelayRefresh()
+	{
+		Input_Is_Delay = false;
+	}
+	
+	public void ActivateInputDelay()
+	{
+		Input_Is_Delay = true;
+		GetNode<Timer>("InputDelay").Start();
+	}
 }
